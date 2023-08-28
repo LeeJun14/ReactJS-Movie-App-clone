d843ea9d534503c86a0b03ceb9f3f25676198a96
LeeJun14 <777ljy777@gmail.com>
2023-08-28 15:26:03 +0900

    To Do List part One

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import userEvent from '@testing-library/user-event';
-import { func } from 'prop-types';
-import {useEffect, useState} from "react";
-
-function Hello(){
-  useEffect(() => {
-    console.log("hi");
-    return() => console.log("bye");
-  }, []);
-  return <h1>Hello</h1>
-}
+import {useState, useEffect} from "react";
 
 function App() {
-  const [showing, setShowing] = useState(0); 
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === ""){
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };    
+  console.log(toDos);
+
   return (
     <div>
-      {showing ? <Hello/> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          onChange={onChange} 
+          value={toDo} 
+          type='text' 
+          placeholder='Write your to do...'>
+        </input>
+        <button>Add To Do</button>
+      </form>
     </div>
+   
   );
 }
 
